48ed4573c490dbf46f36872a9ef95fa21c60ec59
CARNEIRO Renan Graciano Alves <rgcarneiro@lanet.accorservices.net>
2019-03-26 11:21:23 -0300

    Inclusão de configurações de sandbox

--- CurrencyConverter/Conversion.cs
+++ CurrencyConverter/Conversion.cs
@@ -12,17 +12,23 @@ namespace CurrencyConverter
     {
         static void Main(string[] args)
         {
-            string consumerKey = "iPoGLRORfDZckRnCN4Eo1g7rlu_T1uVqxhMUd_Klcc7cdfca!c097a25876bf483ca5c6b3ebf92701770000000000000000";   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
-            string keyAlias = "ConversionKey";   // For production: change this to the key alias you chose when you created your production key - keyalias
-            string keyPassword = "ConvertRenan2019";   // For production: change this to the key alias you chose when you created your production key - testRenan2019
+            string consumerKey = "jzAnk11zzLt0VwgLZSvCmn-leleJcCQglxglnTLBa8ea5038!fe659264cb6d4b71b23fa346c22f54f80000000000000000";   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
+            string keyAlias = "keyalias";   // For production: change this to the key alias you chose when you created your production key - keyalias
+            string keyPassword = "testRenan2019";   // For production: change this to the key alias you chose when you created your production key - testRenan2019
             var path = MasterCard.Core.Util.GetCurrenyAssemblyPath(); // This returns the path to your assembly so it be used to locate your cert
-            string certPath = @"C:\Users\rgcarneiro\Downloads\ConversionKey-production.p12"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
+            string certPath = @"C:\Users\rgcarneiro\Downloads\currency-converter-1553275958-sandbox.p12"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
+
+            //string consumerKey = "iPoGLRORfDZckRnCN4Eo1g7rlu_T1uVqxhMUd_Klcc7cdfca!c097a25876bf483ca5c6b3ebf92701770000000000000000";   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
+            //string keyAlias = "ConversionKey";   // For production: change this to the key alias you chose when you created your production key - keyalias
+            //string keyPassword = "ConvertRenan2019";   // For production: change this to the key alias you chose when you created your production key - testRenan2019
+            //var path = MasterCard.Core.Util.GetCurrenyAssemblyPath(); // This returns the path to your assembly so it be used to locate your cert
+            //string certPath = @"C:\Users\rgcarneiro\Downloads\ConversionKey-production.p12"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
 
             ApiConfig.SetAuthentication(new OAuthAuthentication(consumerKey, certPath, keyAlias, keyPassword));   // You only need to set this once
             ApiConfig.SetDebug(false);   // Enable http wire logging
                                          // This is needed to change the environment to run the sample code. For production: ApiConfig.SetSandbox(false);
                                          //ApiConfig.SetEnvironment((Environment)Enum.Parse(typeof(Environment), "sandbox_mtf".ToUpper()));
-            ApiConfig.SetSandbox(false);
+            ApiConfig.SetSandbox(true);
 
             GetConversion();
 
